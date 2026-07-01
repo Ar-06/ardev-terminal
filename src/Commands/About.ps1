742a961f95339e4ea56be6131e9c980d65a0dc8d
Ernesto Montero <fabiancrack2@gmail.com>
2026-06-30 20:02:11 -0500

    refactor: introduce v1 project architecture

---
+++ src/Commands/About.ps1
@@ -0,0 +1,5 @@
+function Invoke-ArDevAbout {
+    Write-ArDevHeader
+    Write-Host "  ArDev Terminal is a custom developer environment for PowerShell, Windows Terminal and WSL."
+    Write-Host ""
+}
